f91393baf893699cb9dcfebaba2aad14a132b371
GrowthToBetter <baratrahjaga@gmail.com>
2024-08-07 10:53:41 +0700

    tugas PBO bu pasha

--- buPasha/Siswa.java
+++ buPasha/Siswa.java
@@ -5,6 +5,17 @@ public class Siswa{
     double ipk;
     // constructor biasanya sama dengan nama class
     public Siswa(){
-
+        nama="";
+        ipk=0;
+        nis=0;
+    }
+    public void studentSetIpk(double x){
+        ipk=x;
+    }
+    public void studentSetNis(int x){
+        nis=x;
+    }
+    public void studentSetNama(String x){
+        nama=x;
     }
 }
--- buPasha/TesSiswa.java
+++ buPasha/TesSiswa.java
@@ -2,7 +2,24 @@
 
 public class TesSiswa {
     public static void main(String[] args) {
-        Object Siswa = new Siswa();
-        System.out.println(Siswa);
+        // obj
+        Siswa shabriMewing = new Siswa();
+        shabriMewing.studentSetNama("shabri");
+        shabriMewing.studentSetIpk(4.01);
+        shabriMewing.studentSetNis(38203802);
+        Siswa ardianSigma = new Siswa();
+        ardianSigma.studentSetNama("ardian");
+        ardianSigma.studentSetIpk(10.72);
+        ardianSigma.studentSetNis(328398204);
+        Siswa ochiTuru = new Siswa();
+        ochiTuru.studentSetNama("ochi");
+        ochiTuru.studentSetIpk(5.81);
+        ochiTuru.studentSetNis(302830);
+        System.out.println("ini data shabri berupa nama: ipk: nis:");
+        System.out.println(shabriMewing.nama +" " +shabriMewing.ipk +" "+shabriMewing.nis);
+        System.out.println("ini data ardian berupa nama: ipk: nis:");
+        System.out.println(ardianSigma.nama +" " +ardianSigma.ipk +" "+ardianSigma.nis);
+        System.out.println("ini data ochi berupa nama: ipk: nis:");
+        System.out.println(ochiTuru.nama +" " +ochiTuru.ipk +" "+ochiTuru.nis);
     }
 }
