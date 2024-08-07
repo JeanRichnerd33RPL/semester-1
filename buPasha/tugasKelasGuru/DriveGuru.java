8db94469d2f34936224f8ea99b444a4811499748
GrowthToBetter <baratrahjaga@gmail.com>
2024-08-07 14:02:54 +0700

    tugas

---
+++ buPasha/tugasKelasGuru/DriveGuru.java
@@ -0,0 +1,13 @@
+package tugasKelasGuru;
+
+public class DriveGuru {
+    public static void main(String[] args) {
+        Guru objeckGuru= new Guru();
+        // untuk alamat guru saya buat agak acak soalnya gak hafal :)
+        objeckGuru.setGuru("diaur", "Malang", "ipas", 1);
+        objeckGuru.setGuru("Feni", "KedungKandang", "matematika", 2);
+        objeckGuru.setGuru("Andre", "Singosari", "Bahasa Inggris", 3);
+        objeckGuru.setGuru("Pasha", "Malang daerah smp 5", "Produktif RPL", 4);
+        objeckGuru.setGuru("Chusni", "Ijen", "Kesiswaan", 5);
+    }
+}
---
+++ buPasha/tugasKelasGuru/Guru.java
@@ -0,0 +1,18 @@
+package tugasKelasGuru;
+
+public class Guru {
+    String nama,mapel,alamat;
+    int id;
+    public Guru(){
+        nama="";
+        mapel="";
+        alamat="";
+        id=0;
+    }
+    public void setGuru(String n, String a, String m, int i){
+        nama=n;
+        alamat=a;
+        mapel=m;
+        id=i;
+    }
+}
