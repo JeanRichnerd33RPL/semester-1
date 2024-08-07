ca90fc9182d72e2f284571fb38a62204d4b5cac4
GrowthToBetter <baratrahjaga@gmail.com>
2024-08-07 14:07:00 +0700

    tugas bu feni

--- buPasha/tugasKelasGuru/DriveGuru.java
+++ buPasha/tugasKelasGuru/DriveGuru.java
@@ -2,12 +2,21 @@ package tugasKelasGuru;
 
 public class DriveGuru {
     public static void main(String[] args) {
-        Guru objeckGuru= new Guru();
+        Guru pakDiaur= new Guru();
+        Guru buFeni= new Guru();
+        Guru pakAndre= new Guru();
+        Guru buPasha= new Guru();
+        Guru pakChusni= new Guru();
         // untuk alamat guru saya buat agak acak soalnya gak hafal :)
-        objeckGuru.setGuru("diaur", "Malang", "ipas", 1);
-        objeckGuru.setGuru("Feni", "KedungKandang", "matematika", 2);
-        objeckGuru.setGuru("Andre", "Singosari", "Bahasa Inggris", 3);
-        objeckGuru.setGuru("Pasha", "Malang daerah smp 5", "Produktif RPL", 4);
-        objeckGuru.setGuru("Chusni", "Ijen", "Kesiswaan", 5);
+        pakDiaur.setGuru("diaur", "Malang", "ipas", 1);
+        buFeni.setGuru("Feni", "KedungKandang", "matematika", 2);
+        pakAndre.setGuru("Andre", "Singosari", "Bahasa Inggris", 3);
+        buPasha.setGuru("Pasha", "Malang daerah smp 5", "Produktif RPL", 4);
+        pakChusni.setGuru("Chusni", "Ijen", "Kesiswaan", 5);
+        System.out.println(pakDiaur);
+        System.out.println(buFeni);
+        System.out.println(pakAndre);
+        System.out.println(buPasha);
+        System.out.println(pakChusni);
     }
 }
