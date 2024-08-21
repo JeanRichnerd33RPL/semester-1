caeb9dbfb1a755df3626ee903f7cf8fb0c01b782
GrowthToBetter <baratrahjaga@gmail.com>
2024-08-21 15:04:01 +0700

    memindah file

---
+++ buPasha/tugas2/Guru.java
@@ -0,0 +1,15 @@
+package tugas2;
+public class Guru {
+    String nama,mapel,alamat;
+    int id;
+    public Guru(String a, String b, String c, int d){
+        nama=a;
+        mapel=b;
+        alamat=c;
+        id=d;
+    }
+    public void print(){
+        System.out.println("======================================================================");
+        System.out.println("nama : "+nama+" | mapel : "+ mapel +" | alamat : "+alamat +" | id : "+id);
+    }
+}
